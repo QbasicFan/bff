8118871f56a49f87b96f1392004636c1fc525300
QbasicFan <philSerme@gmail.com>
2019-11-30 06:40:47 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class defi(models.Model):
@@ -17,6 +18,24 @@ class defi(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class defiCard(models.Model):
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey(User , related_name='defiCard')
+
+    mr = models.CharField(max_length=400)
+    fr = models.CharField(max_length=400)
+    en = models.CharField(max_length=400)
+    sp = models.CharField(max_length=400)
+
+    son = models.FileField(blank=True, null=True)
+    hasAudio = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
 
 '''
 name | link | type | rating
