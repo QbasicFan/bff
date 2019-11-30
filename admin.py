22d510e330f060aacd5831737059d5f5e882897a
QbasicFan <philSerme@gmail.com>
2019-11-30 06:42:10 -0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import defi, bookMark, catego
+from .models import defi, bookMark, catego, defiCard
 admin.site.register(defi)
 admin.site.register(bookMark)
 admin.site.register(catego)
+admin.site.register(defiCard)
